7e7bb1dd125256c710f059273364770828c4eb57
DirtyDan00 <47438008+DirtyDan00@users.noreply.github.com>
2020-06-29 12:24:41 -0600

    Added Core Files 6/29

--- Game.cpp
+++ Game.cpp
@@ -1,11 +1,28 @@
 // Game.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include <iostream>
+#include "core.h"
 
+#include <iostream>
+bool Update(float dt)
+{
+	return false;
+}
+
+void Draw(Core::Graphics& graphics)
+{
+	graphics.SetColor(RGB(rand() % 256, rand() % 256, rand() % 256)); 
+	graphics.DrawLine(static_cast<float>(rand() % 800), static_cast<float>(rand() % 600), 
+	static_cast<float>(rand() % 800), static_cast<float>(rand() % 600));
+}
 int main()
 {
-    std::cout << "Hello World!\n";
+	char name[] = "CSC196";
+	Core::Init(name, 800, 600);
+	Core::RegisterUpdateFn(Update);
+	Core::RegisterDrawFn(Draw);
+	Core::GameLoop();
+	Core::Shutdown();
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
